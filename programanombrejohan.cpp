fdc1c2522b108833c384393e17725e9923803c98
JohanMusic05 <89704545+electroJohanHe05@users.noreply.github.com>
2021-10-13 15:47:04 +0000

    Initial commit

---
+++ programanombrejohan.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+cout << "johan \n";//Se pone el nombre/
+cout << "Andres\n";//Se pone el segundo nombre/ 
+cout << "Herrera\n";//se pone el primer apellido/
+cout << "Fuentes\n";//se pone el segundo apellido/
+cout << "10-2 B\n";//se pone la seccion 
+}
